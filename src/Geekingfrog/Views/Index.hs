d2af97bb63e7b8d0a948bf705a55f16b730337e0
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2020-09-30 14:03:42 +0100

    typo

--- src/Geekingfrog/Views/Index.hs
+++ src/Geekingfrog/Views/Index.hs
@@ -30,7 +30,7 @@ instance H.ToMarkup Index where
           section ! class_ "container content" $ do
               H.div ! class_ "posts" $ do
                   h2 "Blog"
-                  p "Some of my brain dump about various things, mostly coding, programming languages and open source projects. Here are the most recent posts."
+                  p "Some brain dumps about various things, mostly coding, programming languages and open source projects. Here are the most recent posts."
                   ul ! class_ "posts-overview posts-overview__index" $
                       mapM_ ((li ! class_ "posts-overview--item") . postOverview) (take 5 posts)
               H.div ! class_ "misc" $ do
