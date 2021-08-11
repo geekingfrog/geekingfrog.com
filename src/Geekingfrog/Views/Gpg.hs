e660486da2931fe475602106be3a9e27e7e5d25a
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2021-08-11 17:53:24 +0100

    Update "updated at" -_-

--- src/Geekingfrog/Views/Gpg.hs
+++ src/Geekingfrog/Views/Gpg.hs
@@ -27,7 +27,7 @@ instance H.ToMarkup GpgView where
           h1 ! class_ "main-title" $ "Gpg"
 
       section ! class_ "container content" $ do
-        H.p "Last updated Friday May 1st 2020."
+        H.p "Last updated: July 25th 2021"
         H.p $ (a ! href "https://latacora.micro.blog/2019/07/16/the-pgp-problem.html" $ "This article") <> text " and the fact that I never use gpg conviced me it's not worth keeping a gpg key."
         H.p $ "If you really want to send me something encrypted, I recommend using " <> (a ! href "https://github.com/FiloSottile/age" $ "age") <> ", with the following ssh public key:"
         pre "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJG+Ud7QuiO+AT6hAnPPhJTpGVI9i833/hYAgN4fXL4A"
